362f8178885661401919bb943ff2d11321963802
Your Name <you@example.com>
2018-11-20 06:01:21 +0000

    再提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,12 +14,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to book_path(@book)
+    end
   end
 
   def create
     @new_book = Book.new(book_params)
     @new_book.user_id = current_user.id
     if @new_book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@new_book)
     else
      @books = Book.page(params[:page]).reverse_order
@@ -29,7 +33,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to book_path(@book)
+    end
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book)
     else
     render :edit
@@ -38,6 +46,9 @@ class BooksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to book_path(@book)
+    end
     @book.destroy
     redirect_to books_path(@book)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to user_path(@user)
     else
       # @user = User.find(params[:id])
